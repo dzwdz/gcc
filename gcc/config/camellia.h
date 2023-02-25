3c3daa3852e35cd006546cb8a250a9c9c3eb3c2a
dzwdz <kg67199@gmail.com>
2023-02-25 18:06:36 +0100

    camellia: crtbegin/end

--- gcc/config/camellia.h
+++ gcc/config/camellia.h
@@ -3,3 +3,9 @@
 
 #undef LINK_SPEC
 #define LINK_SPEC "-pie -no-dynamic-linker"
+
+#undef STARTFILE_SPEC
+#define STARTFILE_SPEC "crt0.o%s crti.o%s crtbeginS.o%s"
+
+#undef ENDFILE_SPEC
+#define ENDFILE_SPEC "crtendS.o%s crtn.o%s"
